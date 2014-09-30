f5c1c49f35acab987887852504ea297388de7688
ryelle <kelly.dwan@gmail.com>
2014-09-30 11:01:49 -0400

    Instagram: Move content below the image, and use the permalink to grab the larger image size.

--- views/tggr-source-instagram/shortcode-tagregator-media-item.php
+++ views/tggr-source-instagram/shortcode-tagregator-media-item.php
@@ -1,6 +1,7 @@
 <?php $post = get_post(); ?>
 
-<div id="<?php echo esc_attr( Tagregator::CSS_PREFIX . get_the_ID() ); ?>" class="<?php echo esc_attr( Tagregator::CSS_PREFIX ); ?>media-item <?php echo get_post_type(); ?>">
+<div id="<?php echo esc_attr( Tagregator::CSS_PREFIX . get_the_ID() ); ?>" <?php self::item_class( Tagregator::CSS_PREFIX.'msnry-2' ); ?>>
+
 	<a href="http://instagram.com/<?php echo esc_attr( $author_username ); ?>" class="<?php echo esc_attr( Tagregator::CSS_PREFIX ); ?>author-profile">
 		<?php if ( $author_image_url ) : ?>
 			<img src="<?php echo esc_attr( $author_image_url ); ?>" alt="<?php echo esc_attr( $author_name ); ?>" class="<?php echo esc_attr( Tagregator::CSS_PREFIX ); ?>author-avatar">
@@ -10,18 +11,14 @@
 	</a>
 
 	<div class="<?php echo esc_attr( Tagregator::CSS_PREFIX ); ?>item-content">
-		<?php the_content(); ?>
-
-		<?php if ( $media ) : ?>
-			<?php foreach ( $media as $media_item ) : ?>
-				<?php if ( 'image' == $media_item['type'] ) : ?>
-					<img src="<?php echo esc_url( $media_item['small_url'] ); ?>" alt="" />
-				<?php endif; ?>
-			<?php endforeach; ?>
+		<?php if ( $media_permalink ) : ?>
+			<img src="<?php echo esc_url( $media_permalink . 'media?size=l' ); ?>" alt="" />
 		<?php endif; ?>
+
+		<?php the_content(); ?>
 	</div>
-    
-   	<img class="tggr-source-logo" src="<?php echo esc_attr( $logo_url ); ?>" alt="Instagram" />
+
+	<img class="tggr-source-logo" src="<?php echo esc_attr( $logo_url ); ?>" alt="Instagram" />
 
 	<a href="<?php echo esc_attr( $media_permalink ); ?>" class="<?php echo esc_attr( Tagregator::CSS_PREFIX ); ?>timestamp">
 		<?php echo human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) ) . ' ago'; ?>
