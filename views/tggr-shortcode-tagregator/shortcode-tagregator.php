4c34f09f6db08fc1a6d78248d64fc8c9815efcab
Kelly Dwan <kelly.dwan@gmail.com>
2015-12-06 17:07:53 -0500

    Add logo sources to the javascript settings

--- views/tggr-shortcode-tagregator/shortcode-tagregator.php
+++ views/tggr-shortcode-tagregator/shortcode-tagregator.php
@@ -8,12 +8,19 @@ $media_cpts = array();
 foreach ( Tagregator::get_instance()->media_sources as $source ) {
 	$media_cpts[] = esc_js( $source::POST_TYPE_SLUG );
 };
+$logos = array(
+	'twitter' => plugins_url( 'images/source-logos/twitter.png', dirname( __DIR__ ) ),
+	'instagram' => plugins_url( 'images/source-logos/instagram.png', dirname( __DIR__ ) ),
+	'flickr' => plugins_url( 'images/source-logos/flickr.png', dirname( __DIR__ ) ),
+	'google' => plugins_url( 'images/source-logos/google.png', dirname( __DIR__ ) ),
+);
 ?>
 
 <script type="text/javascript">
 	var tggrData = {
 		ApiUrl:          '<?php echo get_json_url(); ?>',
 		mediaTypes:      <?php echo json_encode( $media_cpts ); ?>,
+		logos:           <?php echo json_encode( $logos ); ?>,
 		hashtag:         '<?php echo esc_js( $attributes['hashtag'] ); ?>',
 		refreshInterval: <?php echo esc_js( $this->refresh_interval ); ?>
 	};
