02ce05b240a88eb11ed9321d8deaa6f078e584fc
Kelly Dwan <kelly.dwan@gmail.com>
2015-12-07 13:24:27 -0500

    Sort items in the store based on the date of the post

--- javascript/stores/media-store.js
+++ javascript/stores/media-store.js
@@ -20,6 +20,11 @@ var _items = [];
  * @param {array} data - array of items, pulled from API
  */
 function _loadItems( data ) {
+	data.sort( function( a, b ){
+		let aDate = new Date( a.date );
+		let bDate = new Date( b.date );
+		return bDate - aDate;
+	} );
 	_items = data;
 }
 
